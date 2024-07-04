d8cd2e44a2b405309819638cd8dcd25d424d56f3
LeeHyungGeol <rjf1138@naver.com>
2024-07-04 15:42:08 +0900

    feat(클래스와-데이터): 클래스가 필요한 이유, 클래스, 클래스 객체 인스턴스 정리

---
+++ java-basic/src/class1/MovieReview.java
@@ -0,0 +1,1 @@
+package class1;public class MovieReview {    public String title;    public String review;    public MovieReview(String title, String review) {        this.title = title;        this.review = review;    }}
---
+++ java-basic/src/class1/ProductOrder.java
@@ -0,0 +1,1 @@
+package class1;public class ProductOrder {    String productName;    int price;    int quantity;    public ProductOrder(String productName, int price, int quantity) {        this.productName = productName;        this.price = price;        this.quantity = quantity;    }}
