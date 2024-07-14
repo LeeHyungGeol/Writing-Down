b2fae56ccbaab635ee14d2c88088bc2756f4d46e
LeeHyungGeol <rjf1138@naver.com>
2024-07-15 02:20:05 +0900

    feat(reflection): spring-di test

--- java-code-manipulation/reflection/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.brothergeol.reflection.reflection {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.brothergeol.reflection.reflection to javafx.fxml;
-    exports com.brothergeol.reflection.reflection;
-}
