63d330df8da51825ebdb4cf6a1a333b066d09535
LeeHyungGeol <rjf1138@naver.com>
2024-07-20 22:59:48 +0900

    feat(java-mid1): Object 클래스

---
+++ java-mid1/src/main/java/lang/object/tostring/ToStringMain.java
@@ -0,0 +1,1 @@
+package lang.object.tostring;public class ToStringMain {    public static void main(String[] args) {        Object object = new Object();        String string = object.toString();        System.out.println("string = " + string);        System.out.println("object = " + object);    }}
