47e75bded5d1010f57cc72561960bc76ea91907f
LeeHyungGeol <rjf1138@naver.com>
2024-09-15 18:34:01 +0900

    feat(java-adv1): 동시성 컬렉션

---
+++ java-mid2/src/main/java/generic/ex1/IntegerBox.java
@@ -0,0 +1,1 @@
+package generic.ex1;public class IntegerBox {    private Integer value;    public Integer get() {        return value;    }    public void set(Integer value) {        this.value = value;    }}
---
+++ java-mid2/src/main/java/generic/ex1/StringBox.java
@@ -0,0 +1,1 @@
+package generic.ex1;public class StringBox {    private String value;    public String get() {        return value;    }    public void set(String value) {        this.value = value;    }}
